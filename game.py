fba958ea97fd957326fd9a1ec712f77717501d93
trumully <truman.mulholland@gmail.com>
2022-02-21 15:53:19 +1300

    Added non-functional virtual keyboard.

--- game.py
+++ game.py
@@ -1,9 +1,20 @@
 import tkinter as tk
-from typing import Optional
+from typing import Optional, Protocol
+
+
+class GameObject(Protocol):
+    def update(self):
+        """Update the game object."""
+
+    def paint(self, canvas: tk.Canvas):
+        """Paints the game object."""
+
 
 class Game:
+    """Game engine base."""
     def __init__(self, title: str, 
                  width: int, height: int, timestep: int = 50):
+        """Constructor for the game engine."""
         self.root = tk.Tk()
         self.root.title(title)
         self.running = False
@@ -17,22 +28,50 @@ class Game:
         self.canvas = tk.Canvas(master=self.frame,
                                 width=width,
                                 height=height,
-                                bg="white"
+                                bg="white",
+                                highlightthickness=0
                                 )
         
         self.canvas.grid(row=0, column=0, rowspan=2, columnspan=1)
 
+        self.objects: list[GameObject] = []
+
+    def add_object(self, object: GameObject):
+        """Add a game object."""
+        self.objects.append(object)
+    
+    def remove_object(self, object: GameObject):
+        """Remove a game object."""
+        self.objects.remove(object)
+
     def run(self):
+        """Start the game."""
         self.running = True
         self._run()
         self.root.mainloop()
 
     def _run(self):
+        """Run the game."""
+        self.update()
+        self.paint()
+
         if self.running:
             self.timer_id = self.root.after(self.timestep, self._run)
 
     def end(self):
+        """End the game."""
         self.running = False
         if self.timer_id is not None:
             self.root.after_cancel(self.timer_id)
         self.root.destroy()
+
+    def update(self):
+        """Update the game."""
+        for obj in self.objects:
+            obj.update()
+
+    def paint(self):
+        """Paints the game."""
+        self.canvas.delete(tk.ALL)  # clear the screen
+        for obj in self.objects:
+            obj.paint(self.canvas)
--- main.py
+++ main.py
@@ -3,6 +3,8 @@ Wordle in Python.
 
 by: Truman
 """
+import tkinter as tk
+
 from game import Game
 from enum import Enum, auto
 
@@ -13,15 +15,22 @@ class WordleState(Enum):
     COMPLETED = auto()
 
 
+class TileState(Enum):
+    BASE = auto()
+    UNCERTAIN = auto()
+    CERTAIN = auto()
+
+
 class Wordle(Game):
-    WINDOW_TITLE = "Wordle"
     WINDOW_SIZE = 500
 
-    def __init__(self):
-        super().__init__(Wordle.WINDOW_TITLE, Wordle.WINDOW_SIZE, 
-                         Wordle.WINDOW_SIZE,
-                        )
+    def __init__(self, title: str = "Wordle", width: int = WINDOW_SIZE, 
+                 height: int = WINDOW_SIZE):
+        super().__init__(title, width, height)
         self.state = WordleState.IDLE
+
+    def initialize(self):
+        self.keyboard = Keyboard()
 
     def set_state(self, state: WordleState):
         self.state = state
@@ -31,10 +40,45 @@ class Wordle(Game):
 
     def paint(self):
         super().paint()
+        self.keyboard.paint(self.canvas)
 
+
+
+class Keyboard:
+    """The keyboard the player interacts with."""
+    QWERTY_KEYBOARD = ["qwertyuiop", "asdfghjkl", "zxcvbnm"]
+    DVORAK_KEYBOARD = ["pyfgcrl", "aoeuidhtns", "qjkxbmvz"]
+    BASE_SIZE = 50
+
+    def __init__(self, keyboard_layout: list = QWERTY_KEYBOARD):
+        self.keyboard_layout = keyboard_layout
+
+    def update(self):
+        pass
+
+    def paint(self, canvas: tk.Canvas):
+        padding = Keyboard.BASE_SIZE
+
+        x = 0
+        y = -padding
+
+        for row in self.keyboard_layout:
+            x = 0
+            y += padding
+
+            for letter in row:
+                canvas.create_rectangle(x, y, 
+                                        x+padding, y+padding, 
+                                        outline="black")
+
+                canvas.create_text(x + (padding / 2), y + (padding / 2), 
+                                   text=letter)
+                x += padding
+            
 
 def main():
     game = Wordle()
+    game.initialize()
     game.run()
 
 
